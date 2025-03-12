18f0866943c3551f558c778ee5ede2112b9fc5ea
ezko-don <[estherzawadi887@gmail.com]>
2025-03-12 02:45:26 -0700

    all

--- new/routes/web.php
+++ new/routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware('auth');
